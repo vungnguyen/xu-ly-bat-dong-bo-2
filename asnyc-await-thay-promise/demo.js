a259cd9a56895329b26bb15455806a8466cb372d
vungnguyen <vungvovi@gmail.com>
2022-07-06 14:28:40 +0700

    p

---
+++ asnyc-await-thay-promise/demo.js
@@ -0,0 +1,30 @@
+async function getDivision(a,b){
+    if(b !== 0) {
+        return a/b;
+    }else{
+        return new Error("Math error")
+    }
+}
+async function f() {
+    try {
+        let result = getDivision(6,2)
+        console.log(result)
+    }catch(e){
+        console.log(e.message)
+    }
+}
+f();
+
+// const promise = (a,b) => {
+//     return new Promise((resolve,reject) => {
+//         setTimeout(() => {
+//             if(b !==0 ){
+//                 resolve(a/b)
+//             }else{
+//                 reject(new Error('Math error'))
+//             }
+//         })
+//     })
+// }
+// promise(2,1).then(x => {console.log(x)})
+// .catch(e => {console.log(e)})
